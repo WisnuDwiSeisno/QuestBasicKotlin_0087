426a1b4566b13a6fa02fc84c32eda0383a858bd0
WisnuDwiSeisno <wseisno37@gmail.com>
2024-10-13 21:26:34 +0700

    membuat function withNamedArgument

--- app/src/main/java/com/example/project1/Functions.kt
+++ app/src/main/java/com/example/project1/Functions.kt
@@ -4,3 +4,17 @@ fun withoutParameter(){
     println("== withoutParameter")
     println("Hello, World!")
 }
+
+fun withParameter(name: String){
+    println()
+    println("== withParameter ==")
+    println("== Hello, $name!")
+}
+
+// named argument
+// named argument merupakan cara untuk mengambil fungsi dengan menyebutkan nama parameter
+fun withNamedArgument(name: String, Age: Int){
+    println()
+    println("== withNamedArgument ==")
+    println("Hello, $name! You are $Age years old.")
+}
