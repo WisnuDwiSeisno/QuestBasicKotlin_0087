917c38f0e0ae5da7fc2dc4327ee81d0113edecae
WisnuDwiSeisno <wseisno37@gmail.com>
2024-10-13 21:39:22 +0700

    membuat function uppercaseString

---
+++ app/src/main/java/com/example/project1/LamdaFunction.kt
@@ -0,0 +1,10 @@
+package com.example.project1
+
+// Lambda Function adalah fungsi yang tidak memiliki nama, biasanya digunakan untuk membuat fungsi sederhana
+// dengan menggunakan -> untuk memisahkan parameter dan body fungsi
+
+fun uppercaseString(string: String): String{
+    return string.uppercase()
+}
+
+// Dapat ditulis dalam ekspresi lambda sebagai berikut:
